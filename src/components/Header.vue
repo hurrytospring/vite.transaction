4dda98dc29d93042d34e893800990a866b0e6a3e
vite-pu <hepu@vite.org>
2018-08-07 09:59:25 +0800

    DEV: 解决ip访问和带#问题

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,13 +4,13 @@
     <br>
     <div class="navMenu">
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#ffffff" text-color="#000000" active-text-color="#3498db">
-        <el-menu-item index="1"><a href="http://localhost:8080" class="nav-item">首页</a></el-menu-item>
+        <el-menu-item index="1"><a href="/" class="nav-item">首页</a></el-menu-item>
         <el-submenu index="2">
           <template slot="title">一级地址</template>
-          <el-menu-item index="2-1"><a href="http://localhost:8080/#/oneLevelAddrDetail" class="nav-item">具体某天交易量</a></el-menu-item>
-          <el-menu-item index="2-2"><a href="http://localhost:8080" class="nav-item">历史交易量排名</a></el-menu-item>
+          <el-menu-item index="2-1"><a href="/oneLevelAddrDetail" class="nav-item">具体某天交易量</a></el-menu-item>
+          <el-menu-item index="2-2"><a href="/" class="nav-item">历史交易量排名</a></el-menu-item>
         </el-submenu>
-        <el-menu-item index="3"><a href="http://localhost:8080/#/marketDetail" class="nav-item">交易所交易详情</a></el-menu-item>
+        <el-menu-item index="3"><a href="/marketDetail" class="nav-item">交易所交易详情</a></el-menu-item>
       </el-menu>
     </div>
     <div class="line"></div>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import MarketDetail from "@/components/MarketDetail"
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
